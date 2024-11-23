8ace7eec72d4bb4f0c7330d685c78922c69afb0d
shubham-chemate <shubhamchemate3@gmail.com>
2024-11-23 17:57:52 +0530

    added queries to add workshop_desc table and inserted some data in it

--- queries.sql
+++ queries.sql
@@ -1,16 +1,45 @@
 CREATE TABLE workshops (
-    id INTEGER,
-    name TEXT,
-    description TEXT,
+    id TEXT,
+    title TEXT,
+    shortDescription TEXT,
     duration INTEGER,
-    fees REAL,
-    max_capacity INTEGER
+    fees INTEGER,
+    maxCapacity INTEGER
 )
 
 INSERT INTO workshops VALUES
-(1, 'Hand Building', 'workshop-desc', 120, 999.0, 4),
-(2, 'Wheel Building', 'workshop-desc', 120, 1399.0, 4)
+('workshop-clay', 'Fun With Clay', 'Create Beautiful Pottery Art Using Clay!', 120, 999, 4),
+('workshop-wheel', 'Fun With Wheel', 'Experience Magic of Wheel and Create Beautiful Pottery Art!', 120, 1399, 4),
+('workshop-clay-and-wheel', 'Fun Together', 'Experience Magic of Hand Clay as well as Wheel Pottery and let your inner article get excited!', 240, 2499, 4)
 
+
+CREATE TABLE WORKSHOP_DESC (
+    workshopId TEXT,
+    workshopDesc TEXT,
+    displayPriority INTEGER
+)
+
+INSERT INTO WORKSHOP_DESC VALUES
+('workshop-clay', 'You will get to craft 1 Piece (usually upto 5 inch) using the magic of your hand!', 1),
+('workshop-clay', 'You can create Plate, Planter, Mug, Pen Stand or any creative art of your own idea', 2),
+('workshop-clay', 'You will get Glazed Product', 3),
+('workshop-clay', 'You will receive your final product within 2 weeks', 4),
+('workshop-clay', 'If you are multiple people attending workshop, please book separately for each',5),
+('workshop-wheel', 'Create your art on wheel - Exclusive wheel pottery!', 1),
+('workshop-wheel', 'You will be able to craft 1 Piece (usually upto 5 inch)', 2),
+('workshop-wheel', 'You can create Plate, Planter, Mug, Pen Stand or any creative art of your own idea', 3),
+('workshop-wheel', 'You will get Glazed Product', 4),
+('workshop-wheel', 'You will receive your final product within 2 weeks', 5),
+('workshop-wheel', 'If you are multiple people attending workshop, please book separately for each', 6),
+('workshop-clay-and-wheel', 'Get deep dive into pottery art with hand and wheel pottery', 1),
+('workshop-clay-and-wheel', 'You will be able to create 2 arts - one with your hand and other on a wheel', 2),
+('workshop-clay-and-wheel', 'You will receive your final product within 2 weeks', 3),
+('workshop-clay-and-wheel', 'If you are multiple people attending workshop, please book separately for each', 4)
+
+SELECT workshopDesc
+FROM WORKSHOP_DESC
+WHERE workshopId='workshop-clay'
+ORDER BY displayPriority
 
 CREATE TABLE bookings (
     id INTEGER,
