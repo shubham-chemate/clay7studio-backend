7372350f331883bd6a88881cd6df9e3651d0aaa8
shubham-chemate <shubhamchemate3@gmail.com>
2024-11-23 17:58:13 +0530

    workshop detail queries

--- db-init.py
+++ db-init.py
@@ -30,7 +30,51 @@ def createWorkshops():
     except Exception as e :
         print(e)
 
-createWorkshops()
+def createWorkshopDescription():
+    conn = sqlite3.connect('data.db')
+    cur = conn.cursor()
+    try:
+        cur.execute("DROP TABLE IF EXISTS WORKSHOP_DESC")
+        conn.commit()
+
+        res = cur.execute("""
+            CREATE TABLE WORKSHOP_DESC (
+                workshopId TEXT,
+                workshopDesc TEXT,
+                displayPriority INTEGER
+            )
+        """)
+
+        cur.execute("""
+            INSERT INTO WORKSHOP_DESC VALUES
+            ('workshop-clay', 'You will get to craft 1 Piece (usually upto 5 inch) using the magic of your hand!', 1),
+            ('workshop-clay', 'You can create Plate, Planter, Mug, Pen Stand or any creative art of your own idea', 2),
+            ('workshop-clay', 'You will get Glazed Product', 3),
+            ('workshop-clay', 'You will receive your final product within 2 weeks', 4),
+            ('workshop-clay', 'If you are multiple people attending workshop, please book separately for each',5),
+            ('workshop-wheel', 'Create your art on wheel - Exclusive wheel pottery!', 1),
+            ('workshop-wheel', 'You will be able to craft 1 Piece (usually upto 5 inch)', 2),
+            ('workshop-wheel', 'You can create Plate, Planter, Mug, Pen Stand or any creative art of your own idea', 3),
+            ('workshop-wheel', 'You will get Glazed Product', 4),
+            ('workshop-wheel', 'You will receive your final product within 2 weeks', 5),
+            ('workshop-wheel', 'If you are multiple people attending workshop, please book separately for each', 6),
+            ('workshop-clay-and-wheel', 'Get deep dive into pottery art with hand and wheel pottery', 1),
+            ('workshop-clay-and-wheel', 'You will be able to create 2 arts - one with your hand and other on a wheel', 2),
+            ('workshop-clay-and-wheel', 'You will receive your final product within 2 weeks', 3),
+            ('workshop-clay-and-wheel', 'If you are multiple people attending workshop, please book separately for each', 4)
+        """)
+
+        conn.commit()
+
+        res = cur.execute("""
+            SELECT workshopDesc
+            FROM WORKSHOP_DESC
+            WHERE workshopId='workshop-clay'
+            ORDER BY displayPriority        
+        """)
+        print(res.fetchall())
+    except Exception as e:
+        print(e)
 
 def createBookings():
     conn = sqlite3.connect("data.db")
@@ -78,3 +122,7 @@ def createSlots():
         print(res.fetchall())
     except Exception as e:
         print(traceback.format_exc())
+
+
+# createWorkshops()
+createWorkshopDescription()
