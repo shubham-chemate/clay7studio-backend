bc91a916a931d13b36e9cf3a857d9cf81216e5e2
shubham-chemate <shubhamchemate3@gmail.com>
2024-11-23 19:10:39 +0530

    date validation

--- app.py
+++ app.py
@@ -38,8 +38,36 @@ def getWorkshops():
         workshop['duration'] = fDuration
     return render_template('workshops.html', workshops=workshops)
 
+# YYYY-MM-DD
+def validateDate(date):
+    [y, m, d] = date.split('-')
+    if int(y)>=2024 and int(y)<=2050 and int(m)>=1 and int(m)<=12 and int(d)>=1 and int(d)<=31:
+        return True
+    return False
+
 @app.route('/workshop/<id>/<workshopDate>', methods=['GET'])
 def getWorkshop(id, workshopDate):
+    print(id, workshopDate)
+    if workshopDate != 'nearest' and not validateDate(workshopDate):
+        return 'bad-request', 400
+    
+    workshopDetails = {
+        'title': 'Fun With Clay',
+        'shortDescription': "Create beatiful pottery art with your hands, don't worry we are there to help you!",
+        'duration': '2 Hrs',
+        'fees': '999/- + GST',
+        'description': [
+            'You will get to craft 1 Piece (usually upto 5 inch)',
+            'You can create Plate, Planter, Mug, Pen Stand or any creative art of your own idea',
+            'You will get Glazed Product',
+            'You will receive your final product within 2 weeks',
+            'If you are multiple people attending workshop, please book separately for each'
+        ],
+        'dates': ['Tue, 25 Nov','Wed, 26 Nov','Thu, 27 Nov','Fri, 28 Nov','Sat, 29 Nov','Sun, 30 Nov'],
+        'selectedDate': 'Wed, 26 Nov',
+        'availableSlots': ['12PM to 2PM', '2PM to 4PM', '4PM to 6PM', '6PM to 8PM']
+    }
+
     workshopDetails = getWorkshopDetails(id, workshopDate)
     if workshopDetails is None or workshopDetails == {}:
         return 'not-found', 404
